e2f652daa120d346795d01a450ea5f67f288463d
Tilman Kamp <tilmankamp@posteo.de>
2017-02-21 18:18:49 +0100

    Better cluster spec

--- example.py
+++ example.py
@@ -1,14 +1,13 @@
 '''
 Distributed Tensorflow 0.8.0 example of using data parallelism and share model parameters.
-Trains a simple sigmoid neural network on mnist for 20 epochs on three machines using one parameter server. 
+Trains a simple sigmoid neural network on mnist for 20 epochs on three machines using one parameter server.
 
-Change the hardcoded host urls below with your own hosts. 
-Run like this: 
+Change the hardcoded host urls below with your own hosts.
+Run like this:
 
-pc-01$ python example.py --job_name="ps" --task_index=0 
-pc-02$ python example.py --job_name="worker" --task_index=0 
-pc-03$ python example.py --job_name="worker" --task_index=1 
-pc-04$ python example.py --job_name="worker" --task_index=2 
+pc-01$ python example.py --job_name="ps" --task_index=0
+pc-02$ python example.py --job_name="worker" --task_index=0
+pc-03$ python example.py --job_name="worker" --task_index=1
 
 More details here: ischlag.github.io
 '''
@@ -19,45 +18,51 @@ import tensorflow as tf
 import sys
 import time
 
-# cluster specification
-parameter_servers = ["pc-01:2222"]
-workers = [	"pc-02:2222", 
-			"pc-03:2222",
-			"pc-04:2222"]
-cluster = tf.train.ClusterSpec({"ps":parameter_servers, "worker":workers})
-
 # input flags
-tf.app.flags.DEFINE_string("job_name", "", "Either 'ps' or 'worker'")
+tf.app.flags.DEFINE_string("task_prefix", "", "A unique task name prefix")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
+tf.app.flags.DEFINE_integer("worker_count", 1, "Amount of workers")
+tf.app.flags.DEFINE_integer("ps_count", 1, "Amount of workers")
 FLAGS = tf.app.flags.FLAGS
 
+# cluster specification
+parameter_servers = ["%s%d.riseml.io:443" % (FLAGS.task_prefix, i) for i in range(FLAGS.ps_count)]
+workers = ["%s%d.riseml.io:443" % (FLAGS.task_prefix, FLAGS.ps_count + i) for i in range(FLAGS.worker_count)]
+
+cluster = tf.train.ClusterSpec({"ps":parameter_servers, "worker":workers})
+
+if FLAGS.task_index >= FLAGS.ps_count:
+	task_index = FLAGS.task_index - FLAGS.ps_count
+	job_name = 'worker'
+else:
+	task_index = FLAGS.task_index
+	job_name = 'ps'
+
 # start a server for a specific task
-server = tf.train.Server(cluster, 
-													job_name=FLAGS.job_name,
-													task_index=FLAGS.task_index)
+server = tf.train.Server(cluster, job_name=job_name, task_index=task_index)
 
 # config
 batch_size = 100
 learning_rate = 0.0005
-training_epochs = 20
+training_epochs = 2000
 logs_path = "/tmp/mnist/1"
 
 # load mnist data set
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-if FLAGS.job_name == "ps":
+if job_name == "ps":
   server.join()
-elif FLAGS.job_name == "worker":
+elif job_name == "worker":
 
 	# Between-graph replication
 	with tf.device(tf.train.replica_device_setter(
-		worker_device="/job:worker/task:%d" % FLAGS.task_index,
+		worker_device="/job:worker/task:%d" % task_index,
 		cluster=cluster)):
 
 		# count the number of updates
-		global_step = tf.get_variable('global_step', [], 
-																initializer = tf.constant_initializer(0), 
+		global_step = tf.get_variable('global_step', [],
+																initializer = tf.constant_initializer(0),
 																trainable = False)
 
 		# input images
@@ -96,16 +101,16 @@ elif FLAGS.job_name == "worker":
 			# optimizer is an "operation" which we can execute in a session
 			grad_op = tf.train.GradientDescentOptimizer(learning_rate)
 			'''
-			rep_op = tf.train.SyncReplicasOptimizer(grad_op, 
+			rep_op = tf.train.SyncReplicasOptimizer(grad_op,
 																					replicas_to_aggregate=len(workers),
- 																					replica_id=FLAGS.task_index, 
+ 																					replica_id=task_index,
  																					total_num_replicas=len(workers),
  																					use_locking=True
  																					)
  			train_op = rep_op.minimize(cross_entropy, global_step=global_step)
  			'''
 			train_op = grad_op.minimize(cross_entropy, global_step=global_step)
-			
+
 		'''
 		init_token_op = rep_op.get_init_tokens_op()
 		chief_queue_runner = rep_op.get_chief_queue_runner()
@@ -120,27 +125,27 @@ elif FLAGS.job_name == "worker":
 		tf.scalar_summary("cost", cross_entropy)
 		tf.scalar_summary("accuracy", accuracy)
 
-		# merge all summaries into a single "operation" which we can execute in a session 
+		# merge all summaries into a single "operation" which we can execute in a session
 		summary_op = tf.merge_all_summaries()
 		init_op = tf.initialize_all_variables()
 		print("Variables initialized ...")
 
-	sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
-														global_step=global_step,
-														init_op=init_op)
+	sv = tf.train.Supervisor(is_chief=(task_index == 0),
+							 global_step=global_step,
+							 init_op=init_op)
 
 	begin_time = time.time()
 	frequency = 100
 	with sv.prepare_or_wait_for_session(server.target) as sess:
 		'''
 		# is chief
-		if FLAGS.task_index == 0:
+		if task_index == 0:
 			sv.start_queue_runners(sess, [chief_queue_runner])
 			sess.run(init_token_op)
 		'''
 		# create log writer object (this will log on every machine)
 		writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
-				
+
 		# perform training cycles
 		start_time = time.time()
 		for epoch in range(training_epochs):
@@ -151,10 +156,10 @@ elif FLAGS.job_name == "worker":
 			count = 0
 			for i in range(batch_count):
 				batch_x, batch_y = mnist.train.next_batch(batch_size)
-				
+
 				# perform the operations we defined earlier on batch
 				_, cost, summary, step = sess.run(
-												[train_op, cross_entropy, summary_op, global_step], 
+												[train_op, cross_entropy, summary_op, global_step],
 												feed_dict={x: batch_x, y_: batch_y})
 				writer.add_summary(summary, step)
 
@@ -162,10 +167,10 @@ elif FLAGS.job_name == "worker":
 				if count % frequency == 0 or i+1 == batch_count:
 					elapsed_time = time.time() - start_time
 					start_time = time.time()
-					print("Step: %d," % (step+1), 
-								" Epoch: %2d," % (epoch+1), 
-								" Batch: %3d of %3d," % (i+1, batch_count), 
-								" Cost: %.4f," % cost, 
+					print("Step: %d," % (step+1),
+								" Epoch: %2d," % (epoch+1),
+								" Batch: %3d of %3d," % (i+1, batch_count),
+								" Cost: %.4f," % cost,
 								" AvgTime: %3.2fms" % float(elapsed_time*1000/frequency))
 					count = 0
 
--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,13 @@
 
 ps_count=1
 worker_count=2
-prefix="_t6_"
+prefix="exclu-1-"
+count=((worker_count+ps_count))
 
 index=0
-while [ "$index" -lt "$worker_count" ]
+while [ "$index" -lt "$count" ]
 do
-  name="worker$prefix$index"
+  name="$prefix$index"
   echo $name
   if [ ! -d "$name" ]; then
     git clone `git remote get-url origin` $name
@@ -19,35 +20,11 @@ do
   git fetch --all
   git rebase origin/master
   cp riseml.yml.template riseml.yml
-  sed -i -e 's/PARAMS/--job_name="worker" --task_index='$index'/g' riseml.yml
+  sed -i -e 's/PARAMS/--worker_count '$worker_count' --ps_count '$ps_count' --task_prefix '$prefix' --task_index='$index'/g' riseml.yml
   git add .
   git commit -m update
-  riseml push
+  riseml push $name
   cd ..
-  echo "Started worker $index."
-  ((index++))
-done
-
-index=0
-while [ "$index" -lt "$ps_count" ]
-do
-  name="ps$prefix$index"
-  echo $name
-  if [ ! -d "$name" ]; then
-    git clone `git remote get-url origin` $name
-    cd $name
-    riseml create
-    cd ..
-  fi
-  cd $name
-  git fetch --all
-  git rebase origin/master
-  cp riseml.yml.template riseml.yml
-  sed -i -e 's/PARAMS/--job_name="ps" --task_index='$index'/g' riseml.yml
-  git add .
-  git commit -m update
-  riseml push
-  cd ..
-  echo "Started parameter server $index."
+  echo "Started task $index."
   ((index++))
 done
